e2f59b6f67be8f0d56272546d9164ad3636945e9
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-06-17 14:52:47 -0300

    create VideoItem class. Organize classes into separeted packages.

---
+++ app/src/main/java/com/example/rodri/mytube/video/VideoItem.java
@@ -0,0 +1,45 @@
+package com.example.rodri.mytube.video;
+
+/**
+ * Created by rodri on 6/17/2016.
+ */
+public class VideoItem {
+
+    private String title;
+    private String description;
+    private String thumbnailURL;
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+    }
+
+}
